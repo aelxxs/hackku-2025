43bbcc05d318a5a297d3d115358c897c1d36d8b1
Vegito2367 <tejaaditya.g@gmail.com>
2025-04-06 00:08:03 -0500

    polishedApproves

--- src/app/api/getapprovalrates/route.ts
+++ src/app/api/getapprovalrates/route.ts
@@ -11,24 +11,31 @@ export async function POST(request: NextRequest) {
         await Promise.all(
             icdCodes.map(async (code: string) => {
                
-                const { data, error } = await supabase.from('commonicd').select().eq('icd', code).limit(1);
+                const { data: approvedData, count: approvedCount } = await supabase
+                    .from('pastapproves')
+                    .select('*', { count: 'exact', head: true })
+                    .eq('icp', code)
+                    .eq('provideID', provider)
+                    .eq('approval', true);
 
-                if (error) {
-                    console.error("Error fetching ICD codes:", error);
-                    return NextResponse.json({ error: "Error fetching ICD codes" }, { status: 500 });
-                }
-                
-                if (data && data.length > 0) {
-                    const approvedRate = Math.floor((Math.random() * 10) + 90);
-                    approvalRates.set(code, approvedRate);
-                    
-                }
-                else{
-                    const approvedRate = Math.floor((Math.random() * 65)+20);
-                    approvalRates.set(code, approvedRate);
-                }
+                const { data: totalData, count: totalCount } = await supabase
+                    .from('pastapproves')
+                    .select('*', { count: 'exact', head: true })
+                    .eq('icp', code)
+                    .eq('provideID', provider);
+
                 console.log("ICD Code: ", code)
-                console.log(data)
+                console.log("Approved Count: ", approvedCount)
+                console.log("Total Count: ", totalCount)
+                console.log("======================")
+                if (totalCount && totalCount > 0) {
+                    // console.log("ICD Code: ", code)
+                    // console.log("Approved Count: ", approvedCount)
+                    // console.log("Total Count: ", totalCount)
+                    // console.log("======================")
+                    const approvalRate = ((approvedCount || 0) / totalCount) * 100;
+                    approvalRates.set(code, approvalRate);
+                }
 
             })
         );
@@ -65,3 +72,23 @@ export async function POST(request: NextRequest) {
                 //     const approvalRate = ((approvedCount || 0) / totalCount) * 100;
                 //     approvalRates.set(code, approvalRate);
                 // }
+
+
+// const { data, error } = await supabase.from('commonicd').select().eq('icd', code).limit(1);
+
+                // if (error) {
+                //     console.error("Error fetching ICD codes:", error);
+                //     return NextResponse.json({ error: "Error fetching ICD codes" }, { status: 500 });
+                // }
+                
+                // if (data && data.length > 0) {
+                //     const approvedRate = Math.floor((Math.random() * 10) + 90);
+                //     approvalRates.set(code, approvedRate);
+                    
+                // }
+                // else{
+                //     const approvedRate = Math.floor((Math.random() * 65)+20);
+                //     approvalRates.set(code, approvedRate);
+                // }
+                // console.log("ICD Code: ", code)
+                // console.log(data)
